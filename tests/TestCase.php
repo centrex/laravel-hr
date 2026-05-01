22a0df79acd1cc5db11443a5657608f1455e4389
rochi88 <29769944+rochi88@users.noreply.github.com>
2026-05-01 13:32:51 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,9 @@ declare(strict_types = 1);
 
 namespace Centrex\Hr\Tests;
 
+use Centrex\Hr\HrServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Centrex\Hr\HrServiceProvider;
 
 class TestCase extends Orchestra
 {
